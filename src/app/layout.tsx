e24b282efb9086aee75d3917a9c1b3e152818ad2
follijulio <follijulio@gmail.com>
2025-08-18 14:15:01 -0300

    Update layout.tsx to enhance site structure and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,24 +2,21 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Partido Urubista",
-  description: "A plataforma do urubismo",
+	title: "Partido Urubista",
+	description: "A plataforma do urubismo",
+	verification: {
+		google: "google54c5b0c84abb80eb.html",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt-br">
-      <body
-        className='antialiased'
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="pt-br">
+			<body className="antialiased">{children}</body>
+		</html>
+	);
 }
-
-
